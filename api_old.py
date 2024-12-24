129c8aeabff2b1d66e248bad090ed0f8b834451c
senrajudson <judsonnascimento.filho@gmail.com>
2024-12-24 01:48:30 -0300

    new lag bot

--- api_old.py
+++ api_old.py
@@ -74,22 +74,26 @@ def lagging_df(df, predict_size):
 
 def forecast_neuralprophet_rolling_with_open_price(df, predict_size=30, lagged_regressor=0, mode="Future"):
     from neuralprophet import NeuralProphet
-
-    cols_toregressor = ['open', 'tick_volume', 'real_volume']
+    model = NeuralProphet()
+    model.device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(torch.cuda.is_available())
 
     if mode == 'Lag':
-        model = NeuralProphet(n_forecasts=predict_size,)
+        ## para usar lagging
+        df = lagging_df(df, predict_size)
+        df.dropna(subset=['y'], inplace=True)
 
-        model.add_lagged_regressor(cols_toregressor)
-
-    if mode == 'Future':
-        model = NeuralProphet()
+    if mode == "Future":
 
         last_date = df['ds'].iloc[-1]
         dates = pd.date_range(start=last_date + pd.Timedelta(minutes=1), periods=predict_size, freq='T')
         future_dates = pd.DataFrame({'ds': dates})
-        future_dates[cols_toregressor] = df[cols_toregressor].iloc[-1].values
+        future_dates['open'] = df['open'].iloc[-1]
+        future_dates['tick_volume'] = df['tick_volume'].iloc[-1]
+        future_dates['real_volume'] = df['real_volume'].iloc[-1]
         future_dates['y'] = None
+
+        # print(future_dates)
 
         df = pd.concat([df, future_dates], ignore_index=True)
 
@@ -97,31 +101,40 @@ def forecast_neuralprophet_rolling_with_open_price(df, predict_size=30, lagged_r
         model.add_future_regressor('tick_volume')
         model.add_future_regressor('real_volume')
 
-    model.device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(torch.cuda.is_available())
-    
-    model.fit(df, freq="1min")
+    if mode == "Lag":
 
-    future = model.make_future_dataframe(
-        df.copy(), periods=predict_size, n_historic_predictions=len(df)
-        )
-    
-    if mode == 'Lag':
-        forecast = model.predict(future)
-    
-    if mode == 'Future':
-        forecast = model.predict(df)
+        model.add_lagged_regressor('open', n_lags=lagged_regressor)
+        model.add_lagged_regressor('tick_volume', n_lags=lagged_regressor)
+        model.add_lagged_regressor('real_volume', n_lags=lagged_regressor)
 
-    yhat_pred = [forecast['yhat1'].iloc[-1] if mode == "Future" else forecast[f'yhat{predict_size}'].iloc[-1]]
-    yhat_pred = round_to_half(yhat_pred[0])
+
+    model.fit(df)
+    forecast = model.predict(df)
+
+    forecast['yhat1'] = forecast['yhat1'].apply(round_to_half)
+    
+    df['yhat1'] = forecast['yhat1'].values
+
+    # print(df.tail(predict_size+5)) 
+
+    yhat_pred = df['yhat1'].iloc[-1] 
+
+    # Removendo as linhas com valores nulos
+    # df.dropna(subset=['y'], inplace=True)
+
     last_diff = yhat_pred - df['y'].iloc[-(predict_size+1)]
     flag = 1 if last_diff > 0 else 0
 
-    return {
+    print(df.tail(predict_size+5))
+
+    result = {
         "flag": flag,
-        "predicted_value": yhat_pred,  # Converte para float nativo do Python
+        "predicted_value": yhat_pred,
         "last_prediction_time": df['ds'].iloc[-1].strftime("%Y.%m.%d %H:%M:%S"),
-    }, df
+    }
+
+    # print(df)
+    return result, df
 
 # Rotas da API
 @app.on_event("startup")
